901d4542fa8fd6575a30c00683540c76d1f8f0f9
Satwinder Singh <satwindersingh97@gmail.com>
2020-05-29 17:06:20 +0530

    Registration Page

--- app/src/main/java/com/example/android/kotlinmessanger/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlinmessanger/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.android.kotlinmessanger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        register_button_registration.setOnClickListener {
+            val email= email_editText_registration.text.toString()
+            val password= password_editText_registration.text.toString()
+
+            Log.d("MainActivity","Email is"+ email)
+            Log.d("MainActivity","Password is $password")
+        }
+
+        already_have_an_account_text_view.setOnClickListener {
+        // launch the login activity
+            val intent= Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
